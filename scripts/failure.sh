685268d82019e5be3f985f4a3ecc1403e1c79428
Mahyuddin Susanto <udienz@gmail.com>
2022-08-19 20:36:56 +0700

    add header token

--- scripts/failure.sh
+++ scripts/failure.sh
@@ -3,7 +3,7 @@
 
 curl "https://api.GitHub.com/repos/udienz/ci-rnd/statuses/$GIT_COMMIT" \
               -H "Content-Type: application/json" \
-              -H "Authorization: $GITHUB_TOKEN" \
+              -H "Authorization: token $GITHUB_TOKEN" \
               -H "Accept: application/vnd.github+json" \
               -X POST \
               -d "{\"state\": \"failure\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"$JENKINS_URL/job/<JenkinsProjectName>/$BUILD_NUMBER/console\"}"
--- scripts/success.sh
+++ scripts/success.sh
@@ -5,6 +5,6 @@ set -x
 curl "https://api.GitHub.com/repos/udienz/ci-rnd/statuses/$GIT_COMMIT" \
               -H "Content-Type: application/json" \
               -H "Accept: application/vnd.github+json" \
-              -H "Authorization: $GITHUB_TOKEN" \
+              -H "Authorization: token $GITHUB_TOKEN" \
               -X POST \
               -d "{\"state\": \"success\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"$JENKINS_URL/job/<JenkinsProjectName>/$BUILD_NUMBER/console\"}"
