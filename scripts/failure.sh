f3ff78353c35f1d3a1534f7ca1ce5afeb5400e95
Mahyuddin Susanto <udienz@gmail.com>
2022-08-19 20:29:55 +0700

    update the sccess and failure

--- scripts/failure.sh
+++ scripts/failure.sh
@@ -4,5 +4,6 @@
 curl "https://api.GitHub.com/repos/udienz/ci-rnd/statuses/$GIT_COMMIT" \
               -H "Content-Type: application/json" \
               -H "Authorization: $GITHUB_TOKEN" \
+              -H "Accept: application/vnd.github+json" \
               -X POST \
               -d "{\"state\": \"failure\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"$JENKINS_URL/job/<JenkinsProjectName>/$BUILD_NUMBER/console\"}"
--- scripts/success.sh
+++ scripts/success.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -x
+
 curl "https://api.GitHub.com/repos/udienz/ci-rnd/statuses/$GIT_COMMIT" \
               -H "Content-Type: application/json" \
+              -H "Accept: application/vnd.github+json" \
               -H "Authorization: $GITHUB_TOKEN" \
               -X POST \
               -d "{\"state\": \"success\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": \"$JENKINS_URL/job/<JenkinsProjectName>/$BUILD_NUMBER/console\"}"
